ca7950499d097d06b582cf2654e06e010422502e
rjthecarguy <rj@rjrobinson.net>
2017-04-08 19:46:33 -0700

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+import { ProfilePage } from '../profile/profile';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+  })
+
+export class HomePage {
+
+
+
+  constructor(public navCtrl: NavController) {
+    
+  }
+
+ goToProfile(){
+    this.navCtrl.push(ProfilePage);
+  }
+
+}
